8403028970f8a76f8a5806e611e6f9ed8c0f12cc
manisman <manish.n.mane@gmail.com>
2018-10-05 07:53:27 +0530

    Summary: comment 1.6

--- SpringDemo/src/main/java/com/example/demo/SpringDemoApplication.java
+++ SpringDemo/src/main/java/com/example/demo/SpringDemoApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringDemoApplication {
 
-	//	Comment added for 1.5
+	//	Comment added for 1.6
 	public static void main(String[] args) {
 		SpringApplication.run(SpringDemoApplication.class, args);
 	}
